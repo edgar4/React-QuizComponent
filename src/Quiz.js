a65cfd3cf98dd4e16b3948e4886af0eb5cb6fe8e
edgar chris <edgarchris4@gmail.com>
2019-11-01 19:13:10 +0300

    try to render quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,8 +2,22 @@ import React, {component} from 'react';
 
 let quizData = require('./quiz_data');
 
-class Quiz extends  component{
+class Quiz extends component {
+
+    constructor(props) {
+        super(props);
+        this.state = {quiz_position: 1};
+
+    }
+
+    render() {
+
+        return '<div>' +
+            '<div class="QuizeQuestion">{{quizData.quiz_questions[0].instruction_text}}</div>'
+            + '</div>';
+    }
 
 }
-export  default Quiz;
+
+export default Quiz;
 
