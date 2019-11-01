04a2b9e555e5eec8f4a357aad7af42d46e69c869
edgar chris <edgarchris4@gmail.com>
2019-11-01 19:42:45 +0300

    Displaying prop data in a list item in the QuizQuestion component

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,16 +4,18 @@ class QuizQuestion extends Component {
 
     render() {
 
-        return <main>
+        return {
+        <main>
             <section>
                 <p>// instruction text goes here</p>
             </section>
             <section className="buttons">
                 <ul>
-                    //quiz question button logic goes here
+                <li>{this.props.quiz_question.answer_options[0].} </li>
                 </ul>
             </section>
         </main>
+    }
     }
 
 }
