cf20e47efdb8741549b1ee05a53e9607c98417fd
edgar chris <edgarchris4@gmail.com>
2019-11-04 19:29:25 +0300

    Displaying prop data in a button tag in the QuizQuestionButton component

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -5,7 +5,7 @@ class QuizQuestionButton extends Component{
     render() {
 
         return(
-            <li><button></button></li>
+            <li><button>{this.props.button_text}</button></li>
         )
     }
 
