118d36e4c19d3710b971230f5ee5d15819d3db7c
edgar chris <edgarchris4@gmail.com>
2019-11-07 00:46:21 +0300

    Adding an event handler to QuizQuestion

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,8 +3,11 @@ import QuizQuestionButton from './QuizQuestionButton.js';
 
 class QuizQuestion extends Component {
 
+    handleClick(buttonText) {
+
+    }
+
     render() {
- console.log(this.props.quiz_question);
         return (
             <main>
                 <section>
@@ -13,7 +16,7 @@ class QuizQuestion extends Component {
                 <section className="buttons">
                     <ul>
                         {this.props.quiz_question.answer_options.map((answer_option, index) => {
-                            return (<QuizQuestionButton key={index} button_text={answer_option}/>)
+                            return (<QuizQuestionButton clickHandler={this.handleClick.bind(this)} key={index} button_text={answer_option}/>)
                         })}
 
                     </ul>
