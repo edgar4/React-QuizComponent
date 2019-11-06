2801d17699b457e615f21f2266c8dc44e103c178
edgar chris <edgarchris4@gmail.com>
2019-11-07 00:48:18 +0300

    Adding an event handler to Quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,13 +13,19 @@ class Quiz extends Component {
 
     }
 
+    showNextQuestion(){
+
+    }
+
     render() {
         const isQuizEnd  = (this.state.quiz_position - 1) === quizData.quiz_questions.length;
 
         return <div>
             {isQuizEnd ? (<QuizEnd/>) :
 
-                (<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>)
+                (<QuizQuestion
+                    showNextQuestionHandler={this.showNextQuestion.bind(this)}
+                    quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>)
             }
 
         </div>
